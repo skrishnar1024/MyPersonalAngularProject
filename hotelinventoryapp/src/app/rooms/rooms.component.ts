4ebcd41375394c8600c8259d855673c61d0bd827
skrishnar1024 <krishnabavanireddy@gmail.com>
2023-03-23 14:01:02 -0500

    Commit

--- hotelinventoryapp/src/app/rooms/rooms.component.ts
+++ hotelinventoryapp/src/app/rooms/rooms.component.ts
@@ -1,18 +1,30 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Room } from './room';
 @Component({
   selector: 'app-rooms',
   templateUrl: './rooms.component.html',
   styleUrls: ['./rooms.component.scss']
 })
-export class RoomsComponent implements OnInit{
+export class RoomsComponent implements OnInit {
 
 hotelName = 'Hilton Hotel';
+numberOfRooms = 10;
+
+rooms : Room={
+  totalRooms: 20,
+  availableRooms: 10,
+  bookedRooms: 5,
+};
+
+hideRooms = false;
 
 constructor(){}
 
-ngOnInit(): void{
-}
+ngOnInit(): void{}
 
+toggle()
+{
+  this.hideRooms = !this.hideRooms;
+}
 
 }
